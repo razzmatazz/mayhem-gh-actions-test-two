c87ac2f743cc5818f542ec6e903fc3ce20501f28
Saulius Menkevičius <saulius@forallsecure.com>
2023-01-04 13:01:16 +0200

    init

---
+++ test.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void fn() {
+    printf("fn\n");
+}
+
+int main(int argc, char** argv)
+{
+    void (*pfn0)(void);
+    char buffer[10];
+    void (*pfn1)(void);
+    int buffer_i = 0;
+    void (*pfn2)(void);
+
+    pfn0 = fn;
+    pfn1 = fn;
+    pfn2 = fn;
+
+    pfn0();
+    pfn1();
+    pfn2();
+
+    while (1) {
+        int c = getchar();
+
+        if (c < 0) {
+            break;
+        }
+
+        buffer[buffer_i++] = c;
+    }
+
+    buffer[buffer_i] = 0; // add terminator
+
+    pfn0();
+    pfn1();
+    pfn2();
+
+    printf("buffer=%s\n", buffer);
+
+    pfn0();
+    pfn1();
+    pfn2();
+
+    return 0;
+}
